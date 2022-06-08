9adb0f4ea4cce5c76ab6f9bf84e8dc8c3c39d3a3
ubayx <107028049+ubayx@users.noreply.github.com>
2022-06-08 10:53:34 +0700

    Initial commit

---
+++ XrootDotDev.sol
@@ -0,0 +1,192 @@
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMWWWWWWWWWWWWWWWWWWWMMMMMWWWWWWWWWWWWWWWWWWWMMWWWWWWWWWWWMMWWWWWWWWWWWMMMMMMMWWWWWWWWWWWWWWWWWWWWWWWMMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMM
+// MMMMMW0l;;;;;;;;;;;;;;;;;;;l0WOc;;;;;;;;;;;;;;;;;;:cc;;;;;;;;;;;::;;;;;;;;;;;l0WMMNkc;;;;;;;;;;;;;;;;;;;;;;:c:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:kNKo:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:xNMMMMMMMM
+// MMMW0o. .,;;;;;;;;;;;;;;;,. ':. .,;;;;;;;;;;;;;;;,.  .,;;;;;;;,.  .;;,;;;,,,. .o0W0, .,;;;;;;;;;;;;;;;;;;;.  .';;;;;;;;,',;;;;;;;;;;;;;;,;' .:, .,;;,;,,;;;;;;;;,;;,;;;'',,;,;;;;;;;;;;;;;;;'..:xXMMMMMM
+// MMMk...,coo:;;;;;;;;;;;coo:'..',col:;;;;;;;;;;;coo:'';oo:;;;:loc,,col:;;;:ooc,...kO'.:ol:;;;;;;;;;;;;;;:lol,',col:;;;coolool;;;;;;;;;;;:lol,'.  'loc;;;;;;;;;;;;;;;;:lollooc;;;;;;;;;;;;;:lol;'. ;kXWMMM
+// MMWd..ldol:. ......... .clodolddol;. ........  'coddoldl.   .;loddol;.   .lxdxl..xO'.:dc.    ......... .;loddlod:.   'oxdxd;    ...... .,lodd:..,oo,    ............'lddddl.    ........  ,loddc...,OMMM
+// MMWd..ox:.   ,dxxxxxd, ...lkkxkx;.   :xxxxxxo.   .lkkxko'     .;dd;.     'okkkd..dO'.ckl.   .lxxxxxxd:   .;xkxkk:    ,dkkkx:    ,dxxxc.  .,oxxxolxx,    ;dxxxxxxxxxxxxkkkkd.   .cxxxxxxc.   'okxxo..dWMM
+// MMWd.'xk:    ;kOOOOOOxooodxOOOOx,    cOOOOOOd'   .lOOOOkdo;    ....    ;odkOOOd'.dO'.lOo.   .dOOOOOOOc.   ,xOOOOc    ,xOOOk:    ;kOOOkdl'  .,oOkkOk;    cOOOOOOOOOOOOOOOOOd'   .lOOOOOOo.   .dOOOx'.dWMM
+// MMWd.'xOc    :O0000000000000000k;    cO00000x'   .o000Oxk0xl:.      .:lx000000x'.d0'.o0o.   .,:::::::'  ,cdO0000l.   ,k000Oc    ;O00000k;    cO000k;    .;::::::::dO000kk0x'    '::::::,  'cok000x'.dWMM
+// MMWd.'k0c    :O0000000000000OO0O;    l000000x'   .o000Oxk0kdl.      .ldk0000kdc..x0'.o0d.    ',,,,,,,. .:oxO0000l.   ;k000Oc    :O00000O;    l0000O;    .,,,,,,,,,oO000xdOx'    .,,,,,,.  ,oxO000k'.dWMM
+// MMWd.'xOc    :O000000OkkkkO0kxOk;    c000000x'   .o0000Okkc.    ..    .:kkOOo'. .k0'.o0o.   .x0OO00OOl.   ;k000Ol.   ,k000Oc    ;O000Okx,  ..lO000k;    cOOOOOOOO000000xdOx'   .o0OO0O0d.   'x000x'.dWMM
+// MMWd.'xO:.   ;kOOOOOk:...'oOOOOk;    cOOOOOOd'   .oOOOOx;.     ,dd,     .;xOkxo..xO'.lOo.   .dOOOOOOOc.   ,xOOOOc.   ,xOOOO:    ;kOOOo'.  'oxkOOOOk;    cOOOOOOOOOOOOOOddOx'   .lOOOOOOo.   .dOOOx'.dWMM
+// MMWd.'dkdl;. .',,'','. .:ldkkkkkoc,. .',,,,,.  .clxkkkkd.   .,cokkoc;.   .okkkd..d0'.ckl.   .okkkkkkkc.   ,dkkkkc    ,xkkkk:    .'','.  ,coxkkkkkkx,    .',,'''',,',;okxxkd'   .lkkkkkkl.   .okkkd'.dWMM
+// MMMx..,:oxd:,,,,,,,,,,,cdxxxxxxodxo;,,,,,,,,,,,cdxxxxxxd:,,,;oxxxxxxo:,,,:oxxxo..dO'.cxo:,,,:dxxxxxxxl;,,,cdxxxxl;,,,cdxxxxl;,,,,,,,,,,;lxxxxxxdoddc,,,,,,,,,,,,,,,,;oxxxxd:,,,;oxxxxxxo:,,,:dxxxo..dWMM
+// MMMXx:. ':loddddddddddddddddolc,;cloddddddddddddddddoccloddddddddolcloddddddddl..d0' ,cloddddddddolccldddddddddoodddddddddoodddddddddddddddddlc;;:coddddddddddddddddddddddooddddddddolcloddddddddl..dWMM
+// MMMMMWk;. .:llllllllllllllll:. .. .cllllllllllllllll;. .:llllllllc' .cllllllll:..dNd,. .cllllllll:. .'cllllllll:;cllllllll::clllllllllllllllc,.....;lllllllllllllllllllllc;:llllllllc' .cllllllll:..dWMM
+// MMMMMMMWO;....................;Ok,..............................................;0MMNk,........................................................d0c.................................................,OMMM
+// MMMMMMMMMN0OOOOOOOOOOOOOOOOOO0NMWX0OOOOOOOOOOOOOOOOOO0K0OOOOOOOOOO000OOOOOOOOOO0XMMMMWX0OOOOOOOOOO0000OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOKWMNKOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO000OOOOOOOOOO0XWMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+pragma solidity 0.8.14;
+// SPDX-License-Identifier: MIT
+import "./ERC721A.sol";
+import "./Ownable.sol";
+
+contract Flying_Cox is ERC721A, Ownable {
+    using Strings for uint256;
+    string public baseURI;
+    string public baseExtension = ".json";
+    uint256 public publicCost = 0.0069 ether;
+    bool public paused = false;
+    uint256 public maxPublic = 10;
+    uint256 public maxAmount = 10;
+    uint256 public maxFree = 1;
+    uint256 public maxSupply = 6969;
+    uint256 public freeQuantity = 2000;
+    uint256 public TeamReserve = 69;
+
+    constructor() ERC721A("Flying Cox", "COX") {
+        setBaseURI("ipfs://QmWD5Z1NYfd8yJEQx6AGt6nKqSVDP1wzuMKtzaQbu8oxvx/");
+    }
+
+    function mint(uint256 quantity) external payable {
+        uint256 supply = totalSupply();
+        require(!paused, "The contract is paused!");
+        require(quantity > 0, "Quantity Must Be Higher Than Zero");
+        require(supply + quantity <= maxSupply, "Max Supply Reached");
+
+        if (msg.sender != owner()) {
+            require(
+                quantity <= maxPublic,
+                "You're Not Allowed To Mint more than maxMint Amount"
+            );
+            if (supply + quantity >= freeQuantity && supply >= freeQuantity) {
+                require(
+                    msg.value >= publicCost * quantity,
+                    "Insufficient Funds"
+                );
+                require(
+                    balanceOf(msg.sender) + quantity <= maxAmount,
+                    "youre not allowed to hold that much"
+                );
+            } else {
+                require(
+                    balanceOf(msg.sender) + quantity <= maxFree,
+                    "youre not allowed to hold that much"
+                );
+                require(
+                    supply + quantity <= freeQuantity,
+                    "Please try with a lower amount to get it for free"
+                );
+            }
+        }
+        _safeMint(msg.sender, quantity);
+    }
+
+    function teamReserve() public onlyOwner {
+        uint256 supply = totalSupply();
+        require(supply + TeamReserve <= maxSupply, "Max Supply Reached");
+        _safeMint(0x09cb60F8C13cf4Dc53105409e035d8294fA3Bbaa, 8);
+        _safeMint(0xDDc7e10242aE8141bc614Eb411412A16Ab8559DA, 8);
+        _safeMint(0xF25F11d09562ab647DB7C8A3C1713Bb4F73ad165, 8);
+        _safeMint(0x7D49eb57c748D3dC1B2710e44485787265Eac655, 8);
+        _safeMint(0x022E224C686f9ae2fd9A0AFd2C5384acaEf9A1Cb, 8);
+        _safeMint(0x82051Aa18952424286c0AE760305f98440B86C5f, 8);
+        _safeMint(0x2A84Fb440dABF67fD36B02db934A9C6F44a04302, 8);
+        _safeMint(0xb040EDFD7d1471e3A01E504fc3d5a722b1981231, 13);
+    }
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
+    function Set(
+        uint256 _publicCost,
+        uint256 _publicMax,
+        uint256 _maxAmount,
+        uint256 _maxFree
+    ) public onlyOwner {
+        publicCost = _publicCost;
+        maxPublic = _publicMax;
+        maxAmount = _maxAmount;
+        maxFree = _maxFree;
+    }
+
+    function setPaused(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function setFreeQuantity(uint256 _freeQuantity) public onlyOwner {
+        freeQuantity = _freeQuantity;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
+        baseExtension = _newBaseExtension;
+    }
+
+    function withdraw() public onlyOwner {
+        (bool ts, ) = payable(owner()).call{value: address(this).balance}("");
+        require(ts);
+    }
+}
+
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKOKWMMMNKXWMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO:,cOWMWx;oKMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXc'ccckNK:;lkNMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx..:lc:xd,;lokNMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd..'cl:;..,coo0WMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXKXWWO,..,coc'.':loxNMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0c:ldkO;..';ll;..,cookNMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWMMMWd'',,'.. ..':c:,..,clokWMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMWOoddddkkko'.....  ..',;::;..,cld0WMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMK:.':lcc:'.......  ....,;:c;..;ldokNMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMXc...',,'.. .''''.......',:c;..;clokNMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMXc. .....  ..'',::,......,;,..,clllkNMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXc.......  ...',:c:,........,:ldl:cOWMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMKo;',,;::'.   ...';::,..'.....':lc:ckWMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc....'''.....  .......,;;;;'...'';lkWMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMWXOdl;.. ...';::,..  .... ...';:::,...;lkWMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMWO:,,;,....'',;;;'...',;;.. ......,::'.,:kWMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMKc...............',:c:;'... ........,cood0NMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMKxl,..',,,'. .';;::,'............';okO00kkXM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMWO,....... ........   ......':llllodxO0xOW
+// MMMMMMMMMMMMMMMMMMMMMMMMMWXOxl'.    ...,;,.. .''... ..''.';;;::cldOkOW
+// MMMMMMMMMMMMMMMMMMMMMMMMKoc;,;'...',;::;,'....,::;,.'cc:,'',':c:;:dxOW
+// MMMMMMMMMMMMMMMMMMMMMMMMx'.'.....';;,''..... ..';clclx0OOkxo:lKXOdclOW
+// MMMMMMMMMMMMMMMMMMMMMMMMKo:,..........   ...  ..',:looodOXWWXXWMMWX0NM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMWXOd:''''',;;,,;;..  ...';:ccloddxxkOXMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMN00kdolclcclcc:clllcc:,... ....',;:cllc;:cxXMMMMM
+// MMMMMMMMMMMMMMMMWNKOdc;'.....',,'',;:;;,,;:c;,,,,'....''',,,;clcdKWMMM
+// MMMMMMMMMMMWXK0kdlcc:;;,'..  ....,;;,,'',::cc:;;::;,'......,:looooONMM
+// MMMMMWNXOkxlccc::::;,'........  ..''...,;;,,;:;,,,;:cc:,...',,codloKMM
+// WXOdlll::;::::;,'......',,::;'..  ....',,'.';::,,''',;::;..''',collOWM
+// oc;,,;::;,''........',:cc:;,'.....   .......'',:;'...'',,,....,;::loKM
+// ;:::;,.....   ...';:;;;;,'.... ..'.         ...',,.....'',,...,;',:cOW//
